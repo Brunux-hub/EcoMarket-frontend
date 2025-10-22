28951178cce614a5e41432ddbe7d1d480a78a8c8
Leonardo <U22223207@utp.edu.pe>
2025-10-22 16:26:56 -0500

    docs(postcss.config.js): document functionality for postcss.config.js  add functional comment

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+// Funcional: postcss.config.js  descripción breve de la responsabilidad principal del archivo.
 module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 }
+
